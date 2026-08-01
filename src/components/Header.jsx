15cc83ae2b98f81fcb9006564a8d934d8ceeb951
sajjadursohag <sajjadursub07@gmail.com>
2026-08-01 21:50:11 +0600

    added date-fns

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import logo from "../assets/logo.png";
+import { format } from "date-fns";
 
 const Header = () => {
   return (
     <div className="flex justify-center flex-col items-center gap-2 ">
       <img className="w-[400px]" src={logo} alt="Logo" />
       <p className="text-accent">Journalism Without Fear or Favour</p>
+      
+          <p className="text-semibold text-accent">{format(new Date(),"EEEE , MMMM MM , yyyy")}</p>
     </div>
   );
 };
